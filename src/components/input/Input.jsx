dc998d288b3bc7004696feeece17f0771db24751
Lucas Bomfim <lucasliko@hotmail.com>
2021-01-25 12:51:01 -0300

    Tasks sendo guardadas no localstorage

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -21,7 +21,6 @@ class Input extends Component {
 
     setTask(task) {
        this.setState({task: task})
-       console.log(this.state.task)
     }
 
     setSecreto(el) {
@@ -34,12 +33,33 @@ class Input extends Component {
 
     saveIt(e) {
       e.preventDefault()
+      let task = [{...this.state}]
+
+      if (localStorage.getItem('tasks') && localStorage.getItem('tasks').length > 0) {
+        let tasksGuardadas = JSON.parse(localStorage.getItem('tasks'))
+        let taskGuardasAtualizadas = [...tasksGuardadas, ...task]
+        
+        localStorage.setItem('tasks', JSON.stringify(taskGuardasAtualizadas))
+      } else {
+        localStorage.setItem('tasks', JSON.stringify(task))
+        console.log('aqui')
+      }
+
+      this.clear()
+    }
+
+    clear() {
+      this.setState({
+        task: '',
+        mensagemSecret: '0',
+        secreto: false
+      })
     }
 
   render() {
      return (
     <form className="todo__form">
-      <input type="text"maxLength={30} className="todo__input" placeholder="Insert your task" onChange={e => this.setTask(e.target.value)}/>
+      <input type="text"maxLength={30} className="todo__input" placeholder="Insert your task" onChange={e => this.setTask(e.target.value)} value={this.state.task}/>
       <input type="submit" value="Save it"className="todo__submit" onClick={this.saveIt}/>
       <img src={this.state.secreto ? secretImgSet : secretImg} alt="secret icon"className={`todo__secret-icon`} onMouseEnter={_ => this.mensagemSecret(true)} onMouseLeave={_ => this.mensagemSecret(false)} onClick={e => this.setSecreto(e.target)}/>
       <p className="todo__secret-msg" style={{opacity: this.state.mensagemSecret}}>Click for insert a {this.state.secreto ? 'normal' : 'secret'} task</p>
