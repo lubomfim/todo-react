b8c03838f4f420209eed7733f91a3a91998bb470
Lucas Bomfim <lucasliko@hotmail.com>
2021-01-25 12:01:31 -0300

    Input style finished

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -16,6 +16,7 @@ class Input extends Component {
       this.mensagemSecret = this.mensagemSecret.bind(this)
       this.setSecreto = this.setSecreto.bind(this)
       this.setTask = this.setTask.bind(this)
+      this.saveIt = this.saveIt.bind(this)
     }
 
     setTask(task) {
@@ -31,13 +32,17 @@ class Input extends Component {
       this.setState({mensagemSecret: bol ? '1' : '0'})
     }
 
+    saveIt(e) {
+      e.preventDefault()
+    }
+
   render() {
      return (
     <form className="todo__form">
       <input type="text"maxLength={30} className="todo__input" placeholder="Insert your task" onChange={e => this.setTask(e.target.value)}/>
-      <input type="submit" value="Save it"className="todo__submit"/>
+      <input type="submit" value="Save it"className="todo__submit" onClick={this.saveIt}/>
       <img src={this.state.secreto ? secretImgSet : secretImg} alt="secret icon"className={`todo__secret-icon`} onMouseEnter={_ => this.mensagemSecret(true)} onMouseLeave={_ => this.mensagemSecret(false)} onClick={e => this.setSecreto(e.target)}/>
-      <p className="todo__secret-msg" style={{opacity: this.state.mensagemSecret}}>Press for insert a secret task</p>
+      <p className="todo__secret-msg" style={{opacity: this.state.mensagemSecret}}>Click for insert a {this.state.secreto ? 'normal' : 'secret'} task</p>
    </form>
     )
   }
